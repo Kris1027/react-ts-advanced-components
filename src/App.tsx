4d8235f6dbe8eef8ba67595a3dfa5539c2be5d9a
Kris1027 <obarzanek.work@gmail.com>
2023-10-29 12:25:37 +0100

    build button flexible component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import Input from './components/Input';
+// import Input from './components/Input';
+import Button from './components/Button';
 
 function App() {
   return (
     <main>
-      <Input id='name' label='Your name' type='text' />
-      <Input id='age' label='Your age' type='number' />
+      <p>
+        <Button>A Button</Button>
+      </p>
+      <p>
+        <Button href='#'>A Link</Button>
+      </p>
     </main>
   );
 }
