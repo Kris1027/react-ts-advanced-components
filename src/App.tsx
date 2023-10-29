5fce5e53afa377006fcff8c5347eea112e83ac99
Kris1027 <obarzanek.work@gmail.com>
2023-10-29 17:59:07 +0100

    form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { useRef } from 'react';
+import Button from './components/Button';
+import Form, { type FormHandle } from './components/Form';
 import Input from './components/Input';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef<FormHandle>(null);
+
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+    customForm.current?.clear();
+  }
 
   return (
     <main>
-      <Input label='test' id='test' ref={input} />
+      <Form onSave={handleSave} ref={customForm}>
+        <Input type='text' label='Name' id='name' />
+        <Input type='number' label='Age' id='age' />
+      </Form>
+      <p>
+        <Button>Save</Button>
+      </p>
     </main>
   );
 }
